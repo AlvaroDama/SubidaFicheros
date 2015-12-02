ea4ae74a344264137e1689f54f63e761da9ef0c2
Álvaro <alvaro.d.jimenez@gmail.com>
2015-12-02 11:17:13 +0100

    Primera subida

---
+++ SubidaFicheros/Controllers/HomeController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Net.Mime;
+using System.Web;
+using System.Web.Mvc;
+using SubidaFicheros.Models;
+using SubidaFicheros.Utiles;
+
+namespace SubidaFicheros.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        List<TipoFichero> tipos = new List<TipoFichero>()
+        {
+            new TipoFichero() {Id = 1, Nombre = "Imagen"},
+            new TipoFichero() {Id = 2, Nombre = "Otra cosa"}
+        };
+        Ficheros_Entities db = new Ficheros_Entities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            var data = db.Ficheros;
+            return View(data);
+        }
+
+        //almacenaje --> determina cómo (dónde) guardaremos:
+        //1) Local
+        //2) 
+        //3) Binary 
+        //4) Nube
+        public ActionResult Subida(int almacenaje = 1)
+        {
+            var tipoAlmacen = "";
+            if (almacenaje == 1)
+                tipoAlmacen = "interno";
+            if (almacenaje == 2)
+                tipoAlmacen = "base64";
+            
+            ViewBag.almacenaje = tipoAlmacen;
+            ViewBag.TipoFichero = new SelectList
+                (tipos, "Id", "Nombre"); // params(lista, valor que guarda, texto del dropbox)
+            return View(new Ficheros());
+        }
+
+        public FileResult DownloadFile(int id, int tipo = 0)
+        {
+            byte[] fichero;
+            var f = db.Ficheros.Find(id);
+            if (tipo == 0)
+            {
+                fichero = Convert.FromBase64String(f.Datos);
+            }
+            else
+            {
+                fichero = f.DatosB;
+            }
+
+            return File(fichero, MediaTypeNames.Application.Octet, f.Nombre);
+        }
+
+        [HttpPost]
+        public ActionResult Subida(Ficheros model, HttpPostedFileBase fichero)
+            // Cada control File del html envía un objeto de tipo HttpPostedFileBase. 
+            // HttpPostedFileBase es un string con t0do el flujo de datos del fichero.
+        {
+            if (model.Tipo == "interno")
+            {
+                var n = GestionarFicheros.GuardarFicheroDisco(fichero, Server);
+
+                if (n != null)
+                {
+                    model.Datos = n;
+                    model.DatosB = new byte[1];
+                    db.Ficheros.Add(model);
+
+                    try
+                    {
+                        db.SaveChanges();
+                    }
+                    catch (DbEntityValidationException e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+            }
+            else if (model.Tipo == "base64")
+            {
+                var data = GestionarFicheros.ToBinario(fichero);
+                if (data != null)
+                {
+                    model.Datos = Convert.ToBase64String(data);
+                    model.DatosB = new byte[1];
+                    model.Nombre = fichero.FileName;
+                    db.Ficheros.Add(model);
+                    try
+                    {
+                        db.SaveChanges();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+            }
+            else if (model.Tipo == "binario")
+            {
+                var datab = GestionarFicheros.ToBinario(fichero);
+                if (datab != null)
+                {
+                    model.Datos = "";
+                    model.DatosB = datab;
+                    model.Nombre = fichero.FileName;
+                    db.Ficheros.Add(model);
+                    try
+                    {
+                        db.SaveChanges();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                }
+            }
+            
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SubidaFicheros/Utiles/GestionarFicheros.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SubidaFicheros.Utiles
+{
+    public class GestionarFicheros
+    {
+        public static string GuardarFicheroDisco(HttpPostedFileBase fichero, 
+            HttpServerUtilityBase server)
+        {
+            var id = Guid.NewGuid(); //--> identificador único
+            // var n = DateTime.Now.Ticks; --> saca la firma horaria.
+            string nombre = null;
+
+            if (fichero != null && fichero.ContentLength > 0)
+            {
+                var ext = fichero.FileName.Substring(fichero.FileName.LastIndexOf(".") + 1);
+                nombre = $"{id}.{ext}"; //--> string.Format("{0}.{1}", id, ext);
+
+                fichero.SaveAs(server.MapPath("/Ficheros") + "/" + nombre);
+            }
+
+            return nombre;
+        }
+
+        public static byte[] ToBinario(HttpPostedFileBase fichero)
+        {
+            byte[] data = null;
+
+            if (fichero != null && fichero.ContentLength > 0)
+            {
+                using (var stream = fichero.InputStream) //Input|Output Stream es genérico.
+                {
+                    var ms = stream as MemoryStream; //nos llevamos el Stream a memoria.
+
+                    if (ms == null)
+                    {
+                        ms = new MemoryStream(); //si no existe, lo forzamos
+                        stream.CopyTo(ms);
+                    }
+
+                    data = ms.ToArray();
+                }
+                
+            }
+
+            return data;
+        }
+    }
+}
